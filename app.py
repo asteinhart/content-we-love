7c7c6e0f4f11fb08ccff460b2094af3738bcd88c
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2024-04-24 21:28:08 -0500

    playing around

--- app.py
+++ app.py
@@ -1,23 +1,51 @@
 from env import KEY
 import requests
-from flask import Flask, render_template, request
+import json
+import pandas as pd
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
 # flask is just allowing me to interact with url parameters
 app = Flask(__name__)
 
+SESSION_TYPE = "filesystem"
+
+Session(app)
+
+article_list = []
+
 
 @app.route("/fetch", methods=["GET"])
-def hello_input():
-    url = "https://api.airtable.com/v0/appfRI9pJ5OIOt9LV/Articles"
+def fetch():
+    url_base = "https://api.airtable.com/v0/appfRI9pJ5OIOt9LV/Articles"
+    global article_list
     headers = {"Authorization": f"Bearer {KEY}"}
+    offset = 0
 
-    response = requests.request("GET", url, headers=headers)
-    articles = response.json()
+    while offset is not None:
+        if offset != 0:
+            url = url_base + "?offset=" + offset
+        else:
+            url = url_base
 
-    return render_template("index.html", articles=articles, data=articles)
+        response = requests.request("GET", url, headers=headers)
+
+        articles = response.json()
+        a = [a["fields"] for a in articles["records"]]
+        article_list += a
+        session["articles"] = article_list
+        offset = articles.get("offset")
+
+    total_articles = len(article_list)
+
+    return render_template(
+        "index.html", articles=article_list, total_articles=total_articles
+    )
 
 
-@app.route("/")
-def get_all_schools():
-
+@app.route("/filter")
+def filter():
+    filter = request.args.get("filter-param")
+    # df = pd.DataFrame(article_list)
+    print(session)
     return render_template("index.html", articles="test", data={})
--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 // inspo https://apps.npr.org/best-books/#view=covers&year=2023
 
-console.log(data)
+//console.log(data)
+
+KEY = (
+    "patzYrMMVCYJOlcXE.60d2aac94cf2976072700a68b61ae1040f18b9c9da08f5ebacda2a2d4a791ee2"
+)
+
+
+var Airtable = require(['airtable']);
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: KEY
+});
+var base = Airtable.base('appfRI9pJ5OIOt9LV');
+
+console.log(base)
